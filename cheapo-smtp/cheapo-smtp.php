7f9c1d8f4e8aebe9850eb1fe053f9b24455514a7
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2025-12-12 12:06:57 -0800

    Update cheapo-smtp plugin header with standard metadata

--- cheapo-smtp/cheapo-smtp.php
+++ cheapo-smtp/cheapo-smtp.php
@@ -1,16 +1,23 @@
 <?php
 /**
  * Plugin Name: Cheapo SMTP
- * Description: SMTP config via phpmailer_init.
- * 
- * Add the following to wp-config.php:
- * 
- * define( 'SMTP_USER',  'your_username' );
- * define( 'SMTP_TOKEN', 'your_token_or_password' );
- * define( 'SMTP_HOST',  'your_host' );
- * define( 'SMTP_PORT',  587 ); // TLS port; or set to 465 for SSL
- * define( 'SMTP_FROM',  'your_from_email' );
- * define( 'SMTP_NAME',  'your_from_name' );
+ * Plugin URI: https://github.com/ironprogrammer/mu-plugins/tree/main/cheapo-smtp
+ * Description: SMTP configuration via phpmailer_init hook.
+ * Author: Brian Alexander
+ * Author URI: https://brianalexander.com
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Version: 1.0.0
+ *
+ * Configuration:
+ *   Add the following constants to wp-config.php:
+ *
+ *   define( 'SMTP_USER',  'your_username' );
+ *   define( 'SMTP_TOKEN', 'your_token_or_password' );
+ *   define( 'SMTP_HOST',  'your_host' );
+ *   define( 'SMTP_PORT',  587 ); // TLS port; or set to 465 for SSL
+ *   define( 'SMTP_FROM',  'your_from_email' );
+ *   define( 'SMTP_NAME',  'your_from_name' );
  */
 
 add_action( 'phpmailer_init', 'setup_phpmailer_init' );
